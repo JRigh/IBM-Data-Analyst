0a7dd46b9771445592a052a9fedf4eeac5894a74
Julian <julian.righ.sampedro@gmail.com>
2023-03-24 13:16:12 +0100

    Add files via upload

---
+++ 4. Python for Data Science, AI & Development/basic Python.py
@@ -0,0 +1,36 @@
+#--------------
+# Python basics
+#--------------
+
+
+# check what version of Python we are using
+import sys
+print(sys.version)
+
+# latest (3.10.2) march 2023
+
+# type of object (equivalent to class() in R)
+type(2.14)
+type(True)
+type(1)
+type('string')
+
+# system settings about float type
+sys.float_info
+
+# Convert an integer to a string
+str(2)
+
+# integer division //
+7 // 2
+
+# expressions
+43 + 60 + 16 + 41
+
+# variables
+x = 43 + 60 + 16 + 41
+x
+
+#----
+# end
+#----
